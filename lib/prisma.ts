7f414c70b7cd98bf4b368d970d56142e23ebe6b3
vldclp <vldclp@vldclps-MacBook-Pro.local>
2026-04-10 18:30:07 +0300

    Updated prisma version

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 const prisma =
   globalForPrisma.prisma ||
